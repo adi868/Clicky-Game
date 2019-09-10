b561406882c9a92cd6d03285ae8ad6ca455ff134
alciancio <alcianciotto@gmail.com>
2019-09-10 06:19:46 -0500

    Text Update

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -4,7 +4,7 @@ import "./style.css";
 function Footer() {
   return (
     <footer className="footer">
-    <div className="footer-copyright text-center py-3">Pokemon Clicky-Game
+    <div className="footer-copyright text-center py-3">Pokémon Clicky-Game
     </div>
   </footer>
   )
--- client/src/components/NavBar/index.js
+++ client/src/components/NavBar/index.js
@@ -6,7 +6,7 @@ function NavBar(props) {
         <nav className="navbar sticky-top navbar-expand">
             <ul className="navbar-nav">
                 <li className="nav-item">
-                    <h1>Pokemon Clicky-Game</h1><span className="sr-only nav-link title">(current)</span>
+                    <h1>Pokémon Clicky-Game</h1><span className="sr-only nav-link title">(current)</span>
                 </li>
                 <li className="nav-item">
                     <h4 className={`nav-link ${props.colorStyle}`} >{props.mid}</h4>
