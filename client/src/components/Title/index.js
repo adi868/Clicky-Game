91723ede2fc1dad457c4772929a909abd24ef753
alciancio <alcianciotto@gmail.com>
2019-09-11 13:46:25 -0500

    Styling Changes BGM Editing

--- client/src/components/Title/index.js
+++ client/src/components/Title/index.js
@@ -5,8 +5,10 @@ function Title(props) {
   return (
     <div className="jumbotron jumbotron-fluid">
         <div className="container text-align">
-            <img src="/images/squirtle.png" id="ash" alt="Ash Ketchum" data-toggle="popover" data-placement="top" data-content="Hey Poke-Trainer! Try and Catch Them All (But Don't Do It Twice!)" />
+            <img src="/images/squirtle.png" id="ash" alt="Ash Ketchum" data-toggle="tooltip" data-placement="top" data-content="Hey Poke-Trainer! Try and Catch Them All (But Don't Do It Twice!)" />
             <span id="hover">Hey There!</span>
+            <span id="clickMe">Click Me!</span>
+            <input type="button" id = "play" value="PLAY MUSIC" onclick="play()"></input>
         </div>
         {props.children}
     </div>
